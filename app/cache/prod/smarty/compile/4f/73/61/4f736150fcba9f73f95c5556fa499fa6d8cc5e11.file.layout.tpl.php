f40bddaf9f8b8d8e27a7577b6827eedd0c9f0d06
raphael <moudens.raphael@orange.fr>
2017-07-21 16:28:13 +0200

    ajout texte

---
+++ app/cache/prod/smarty/compile/4f/73/61/4f736150fcba9f73f95c5556fa499fa6d8cc5e11.file.layout.tpl.php
@@ -0,0 +1,36 @@
+<?php /* Smarty version Smarty-3.1.19, created on 2017-07-21 11:44:56
+         compiled from "/home/raphael/exercice/amazon/admin024mqh0f7/themes/default/template/controllers/login/layout.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:14079230665971cd18103f15-51905252%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '4f736150fcba9f73f95c5556fa499fa6d8cc5e11' => 
+    array (
+      0 => '/home/raphael/exercice/amazon/admin024mqh0f7/themes/default/template/controllers/login/layout.tpl',
+      1 => 1500369157,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '14079230665971cd18103f15-51905252',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'header' => 0,
+    'page' => 0,
+    'footer' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_5971cd18107dc4_35296224',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5971cd18107dc4_35296224')) {function content_5971cd18107dc4_35296224($_smarty_tpl) {?>
+
+<?php echo $_smarty_tpl->tpl_vars['header']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['page']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['footer']->value;?>
+
+<?php }} ?>
